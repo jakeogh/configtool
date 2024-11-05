e20b6f1c48fe60b1a41160a3cde1c4b4e707a4d3
jakeogh <github.com@v6y.net>
2024-11-04 18:47:35 -0700

    auto-commit

--- configtool/__init__.py
+++ configtool/__init__.py
@@ -1,7 +1,12 @@
-from .configtool import click_read_config
-from .configtool import click_remove_config_entry
-from .configtool import click_write_config_entry
-from .configtool import get_config_directory
-from .configtool import get_data_dir
-from .configtool import read_config
-from .configtool import write_config_entry
+# -*- coding: utf-8 -*-
+"""
+isort:skip_file
+"""
+
+from .configtool import click_read_config as click_read_config
+from .configtool import click_remove_config_entry as click_remove_config_entry
+from .configtool import click_write_config_entry as click_write_config_entry
+from .configtool import get_config_directory as get_config_directory
+from .configtool import get_data_dir as get_data_dir
+from .configtool import read_config as read_config
+from .configtool import write_config_entry as write_config_entry
